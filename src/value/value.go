7cafb8b87ff206408520ba7c0f31da34711cf08f
ssoda <demaciasoda@gmail.com>
2019-07-30 22:49:17 +0800

    [New] value

---
+++ src/value/value.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("OK")
+	fmt.Println("go" + "101")
+	fmt.Println(1 + 1)
+	fmt.Println(0.3 + 0.4)
+	fmt.Println(1 / 0.3)
+	fmt.Println(true && false)
+}
