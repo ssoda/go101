9f5a156a22d797685eedd0a8a12dda267400f77c
ssoda <demaciasoda@gmail.com>
2019-07-31 22:00:21 +0800

    [New] Variables

---
+++ src/variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "this is a"
+	fmt.Println(a)
+	b := "this is b"
+	fmt.Println(b)
+	fmt.Println(a + " " + b)
+	c := 3
+	fmt.Println(c)
+	var d, e int = 4, 5
+	fmt.Println(c + d)
+	fmt.Println(d + e)
+}
