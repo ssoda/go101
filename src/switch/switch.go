809024a1309f83196424c832f119d135c72e0c10
ssoda <demaciasoda@gmail.com>
2019-08-11 00:25:27 +0800

    [New] Switch

---
+++ src/switch/switch.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+}
